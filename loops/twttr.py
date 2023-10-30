1ebeb79425c8d48ea54a7a3ce7b9c7b5e1cf490b
valeriahhdez <valeriahhdez@gmail.com>
2023-10-29 21:27:11 -0600

    Update twttr

--- loops/twttr.py
+++ loops/twttr.py
@@ -5,13 +5,10 @@ vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
 
 def main():
     rmv_vowels(input_str)
-    
-
 
 def rmv_vowels(c):
     no_vowels =''.join([l for l in c if l not in vowels])
     print(no_vowels)
-
 
 main()
 
