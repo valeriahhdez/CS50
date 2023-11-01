8731d6e65f82338932225b3ecaa8071f0b8a2118
valeriahhdez <valeriahhdez@gmail.com>
2023-11-01 11:03:23 -0600

    Add documentation

--- loops/plates.py
+++ loops/plates.py
@@ -1,49 +1,82 @@
-# To check if the plate number contains a punctuation character, import punctuation
-from string import punctuation
 
+from string import punctuation
+# Minimum length of plate
 l_min = 2
+# Max length of plate
 l_max = 6
 
 
 def main():
+    """
+    Prompts the user to give a string for vanity plate and calls is_valid to evaluate the string. 
+    Expects: plate->string
+    Modifies: nothing
+    Returns: VALID if is_valid is True, INVALID if it doesn't. 
+    """
     plate = input("Plate: ")
     if is_valid(plate):
         print("Valid")
     else:
         print("Invalid")
 
-def is_valid(input_str):
+
+def is_valid(input_str: str) ->bool:
+    """
+    Loops over a list of conditions and cheks if they are all met 
+    Expects: 
+    input_string
+    Modifies: 
+    nothing
+    Returns: 
+    True if all conditions are met, False otherwise. 
+    """
+
+    # Check if plate starts with two letters at least and store it vairable
     start_with_letters = not(any(char.isdigit() for char in input_str[0:2]))
+    # Check if plate contains 0 at first position and store it in variable
     nostart_with_0 = not('0' in input_str[0])
 
-    # List of conditions to be met
+    
     conditions = [
-    # Check if plates start with at least two letters
+    
     start_with_letters,
-    # Check valid length  
-    has_length(input_str, l_min, l_max),
-    # Check if string contains 0 at string[0]
     nostart_with_0,
+    # Check valid length  
+    has_length(input_str, l_min, l_max),    
     # Check if numbers are in the middle of the string
     middle_numbers(input_str),
     # Check if string contains punctuation marks
     has_punctuation(input_str)
 ]
+    # Check if each condition is True
     for condition in conditions:
         if not condition:
             return False
         else: 
             return True
 
-def has_length(x_str, l_min, l_max):
-    l_min <= len(x_str) <=l_max
+def has_length(x_str, m, x):
+    """
+    Checks is the plate has valid length
+    Expects: 
+    x_str: vanity plate
+    m: minimum length
+    x: maximum length
+    Modifies:
+    Nothing
+    Returns:
+    True is length of vanity plate is between 2 and 6, False if othwerwise.  
+    """
+    m <= len(x_str) <= x
 
 
-def has_punctuation(str):
-    # punctuation = ['.', '?', '!', ':', ';', ',']
-    if any(s in str for s in punctuation):
+def has_punctuation(s: str) -> bool:
+    """
+    Checks if the vanity plate string contains any punctuation mark or space
+    """
+    if any(p in s for p in punctuation):
         return False
-    elif ' ' in str: 
+    elif ' ' in s: 
         return False
     else:
         return True
@@ -52,13 +85,7 @@ def has_punctuation(str):
 
 def middle_numbers(y):
     """
-   Splits string in two parts at position of first encountered number. 
-   Checks if the second part of the string contains only numbers. If so, it meets the condition.
-   Args:
-    y: A string representing the vanity plate.
-
-  Returns:
-    True if the string does not contain numbers in the middle, False otherwise.
+   Returns True if 
     """
     number_index = None
     for i in range(len(y)):
