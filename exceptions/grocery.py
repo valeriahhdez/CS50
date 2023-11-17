91bcbd832c3cadece38122c25e41dd493f19beb5
valeriahhdez <valeriahhdez@gmail.com>
2023-11-16 21:19:39 -0600

    Add counting list items and create dict

--- exceptions/grocery.py
+++ exceptions/grocery.py
@@ -1,4 +1,14 @@
+# Prompt the user to enter an item
+
+# Add item to list
+
+# When user types crtl + d, count the number of times 
+# each times appears in the list and store the results in a dictionary
+
+# print the contents of dictionary
+
 grocery_list = []
+grocery_dict = {}
 while True:
 
     try:
@@ -8,7 +18,11 @@ while True:
         
            
     except EOFError:
+        
+        for item in range(len(grocery_list)):
+            grocery_dict[grocery_list[item]] = grocery_list.count(grocery_list[item])
+
+        print(grocery_dict)
+        
         exit()
         ... 
-
-    print(grocery_list)
