af5156bd6292ed39cb0bd7df17b612537ad0c4a2
valeriahhdez <valeriahhdez@gmail.com>
2023-11-17 21:56:11 -0600

    Add preliminary solution

--- exceptions/grocery.py
+++ exceptions/grocery.py
@@ -1,28 +1,27 @@
-# Prompt the user to enter an item
+""" This code prompts the user to enter an item
 
-# Add item to list
+Adds the item to a list
 
-# When user types crtl + d, count the number of times 
-# each times appears in the list and store the results in a dictionary
+When user types crtl + d, it counts the number of times 
+each item was entered stores the information in a dictionary
 
-# print the contents of dictionary
+Finally, it prints the contents of dictionary"""
 
 grocery_list = []
 grocery_dict = {}
 while True:
-
-    try:
-         
+    try: 
         grocery_item = input("")
         grocery_list.append(grocery_item)
-        
-           
+              
     except EOFError:
         
         for item in range(len(grocery_list)):
             grocery_dict[grocery_list[item]] = grocery_list.count(grocery_list[item])
 
-        print(grocery_dict)
+
+        for key, value in grocery_dict.items():
+            print(value, key.upper())
         
         exit()
         ... 
