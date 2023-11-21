44e5d37ee12fd37c6f511ce116540d0fc9af449d
valeriahhdez <valeriahhdez@gmail.com>
2023-11-21 16:40:15 -0600

    Add functions

--- exceptions/outdated.py
+++ exceptions/outdated.py
@@ -1,15 +1,76 @@
-# Prompt the user to enter a date
-input_date = input("Date: ")
 
-date_i = input_date.split('/')
+month_numbers = list(range(1,13))
+month_names = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+]
 
-month_i = date_i[0]
-day_i = date_i[1]
-year_i = date_i[2]
+days_month = list(range(1,31))
 
-# Separate values by '/'
+
+def valid_month(x, y,z): 
+    """ 
+    Checks if the month given is month_names or in month_numbers
+    Args: month_names, month_numbers, month_item
+    Changes: nothing
+    Returns: True if month_item is valid 
+    """
+    # check if month_item is month_names list
+    if x in y:
+        return True
+    # check if month_item is month_numbers list
+    elif x in z:
+        return True
+    else: 
+        return False
+    
+    
+def valid_day(x, y): 
+    if x in y:
+        return True
+    else: 
+        return False
+
+def valid_date(m,n):
+    if valid_day() and valid_month:
+        return True
+    else: 
+        return False
+
+def double_digit_month(v):
+    if v.isdigit():
+        return (str(v).zfill(2))
+    else: 
+        return v   
+
+while true:
+    try:
+        # Prompt the user to enter a date
+        input_date = input("Date: ")
+        # Separate values by '/'
+        date_list = input_date.split('/')
+
+        month_item = date_list[0]
+        day_item = date_list[1]
+        year_item = date_list[2]
+        if valid_date:
+            # add a zero at the begining of month (if number) and day if any or both are single digits. 
+
+
 # Check if the input is valid,
-# either 8/7/1987 or August/7/1987 are valid inputs.
+# month has to be in the list o be within 1-12 interval
+# day has to be within the 1-31 range
+
 
 # Propmt the user again if input isn't valid. 
 
@@ -20,6 +81,6 @@ year_i = date_i[2]
 
 # Print input[2] + '/'+ input[1] + '/' + input[0]
 
-x = list(range(1,13))
 
-print(y)
+
+# print(y)
