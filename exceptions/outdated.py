a61e573f439b7d820e02da474ad9cff0b30601e0
valeriahhdez <valeriahhdez@gmail.com>
2023-11-20 19:29:54 -0600

    Add outdated.py  pseudocode

---
+++ exceptions/outdated.py
@@ -0,0 +1,25 @@
+# Prompt the user to enter a date
+input_date = input("Date: ")
+
+date_i = input_date.split('/')
+
+month_i = date_i[0]
+day_i = date_i[1]
+year_i = date_i[2]
+
+# Separate values by '/'
+# Check if the input is valid,
+# either 8/7/1987 or August/7/1987 are valid inputs.
+
+# Propmt the user again if input isn't valid. 
+
+# Check if input[0] in range(1,13) or input[0] in months_list
+
+# If the first two values of input are numbers and they are single numbers, 
+# add a 0 at the beginning
+
+# Print input[2] + '/'+ input[1] + '/' + input[0]
+
+x = list(range(1,13))
+
+print(y)
