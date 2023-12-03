58ee7982b1b1f3779ea581ae40a013b29948d872
valeriahhdez <valeriahhdez@gmail.com>
2023-12-02 18:29:11 -0600

    Add generate_random_int function

--- libraries/professor.py
+++ libraries/professor.py
@@ -1,20 +1,13 @@
-# Prompt the user
 
-# Check if answer is 1,2, or 3
-# If input is invalid, reprompt
 # If valid, store the input in a variable n
 
 # Generate two random numbers (r_ number1, r_number2) with n digits
 
-# Print r_number1 + r_number2
+# Print r_number1 + r_number2 and prompt the user for answer
 
-# Check the ansert of the user
-
-# If answer input is not correct or is not a number, reprompt
+# If answer is not correct or is not a number, reprompt
 
 # Count the number of attempts, after three attempts, print the correct answer and print the next math problem 
-
-
 
 
 import random
@@ -38,18 +31,39 @@ def get_level():
         try:
             level_input = int(level_input)
             if level_input in range(1,4):
-                return True
-                
+                return True 
             else:
                 continue
                 
         except ValueError:
             continue
 
+def generate_random_integer(n: int) -> int:
+    """
+    Generates a random integer with exactly 'n' digits.
 
+    Args:
+    - n: int
+        The number of digits the generated random integer should have.
 
-def generate_integer(level):
-    ...
+    Returns:
+    - int:
+        A random integer with exactly 'n' digits.
+
+    """
+
+    # Validating the input 'n'
+    if not isinstance(n, int) or n <= 0:
+        raise ValueError("n should be a positive integer.")
+
+    # Generating the lower and upper bounds for the random integer
+    lower_bound = 10 ** (n - 1)  # Smallest n-digit number
+    upper_bound = (10 ** n) - 1  # Largest n-digit number
+
+    # Generating the random integer within the specified range
+    random_integer = random.randint(lower_bound, upper_bound)
+
+    return random_integer
 
 
 if __name__ == "__main__":
