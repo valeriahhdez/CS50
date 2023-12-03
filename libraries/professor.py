9d98358bf2bb63c509d48e194ddeeaf477e98526
valeriahhdez <valeriahhdez@gmail.com>
2023-12-03 16:12:40 -0600

    Add tested code

--- libraries/professor.py
+++ libraries/professor.py
@@ -1,20 +1,41 @@
-
-# If valid, store the input in a variable n
-
-# Generate two random numbers (r_ number1, r_number2) with n digits
-
-# Print r_number1 + r_number2 and prompt the user for answer
-
-# If answer is not correct or is not a number, reprompt
-
-# Count the number of attempts, after three attempts, print the correct answer and print the next math problem 
-
-
 import random
 
 
 def main():
-    ...
+    level = get_level()
+    score = 0
+    
+
+    for _ in range(10):
+        number_1 = generate_random_integer(level)
+        number_2 = generate_random_integer(level)
+        
+        attempts = 0
+        while attempts < 4:
+            attempts += 1
+            correct_answer = number_1 + number_2
+            show_answer = f"{number_1} + {number_2} = {correct_answer}"
+            try:
+                answer = int(input(f"{number_1} + {number_2} = "))
+                if answer == correct_answer:
+                    score += 1
+                    break
+                else:
+                    if attempts == 3:
+                        print(show_answer)
+                        break
+                    else:
+                        print('EEE')
+                        continue
+
+            except ValueError:
+                if attempts == 3:
+                    print(show_answer)
+                    break
+                else:
+                    print('EEE')
+                    continue
+    print(f"Score: {score}")
 
 
 def get_level():
@@ -31,42 +52,53 @@ def get_level():
         try:
             level_input = int(level_input)
             if level_input in range(1,4):
-                return True 
+                return level_input 
             else:
                 continue
                 
         except ValueError:
             continue
 
-def generate_random_integer(n: int) -> int:
-    """
-    Generates a random integer with exactly 'n' digits.
+# def generate_random_integer(n: int) -> int:
+#     """
+#     Generates a random integer with exactly 'n' digits.
 
-    Args:
-    - n: int
-        The number of digits the generated random integer should have.
+#     Args:
+#     - n: int
+#         The number of digits the generated random integer should have.
 
-    Returns:
-    - int:
-        A random integer with exactly 'n' digits.
+#     Returns:
+#     - int:
+#         A random integer with exactly 'n' digits.
 
-    """
+#     """
 
-    # Validating the input 'n'
-    if not isinstance(n, int) or n <= 0:
-        raise ValueError("n should be a positive integer.")
+#     # Validating the input 'n'
+#     # if not isinstance(n, int) or n <= 0:
+#     #     raise ValueError("n should be a positive integer.")
 
-    # Generating the lower and upper bounds for the random integer
-    lower_bound = 10 ** (n - 1)  # Smallest n-digit number
-    upper_bound = (10 ** n) - 1  # Largest n-digit number
+#     # Generating the lower and upper bounds for the random integer
+#     lower_bound = 10 ** (n - 1)  # Smallest n-digit number
+#     upper_bound = (10 ** n) - 1  # Largest n-digit number
 
-    # Generating the random integer within the specified range
-    random_integer = random.randint(lower_bound, upper_bound)
+#     # Generating the random integer within the specified range
+#     random_integer = random.randint(lower_bound, upper_bound)
 
-    return random_integer
+#     return random_integer
+
+def generate_random_integer(n):
+    level_ranges = {
+        1: (0, 9),
+        2: (10, 99),
+        3: (100, 999)
+    }
+
+    if n not in level_ranges:
+        raise ValueError("Invalid level")
+
+    min_value, max_value = level_ranges[n]
+    return random.randint(min_value, max_value)
 
 
 if __name__ == "__main__":
     main()
-
-
