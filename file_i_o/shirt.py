3ebbb36b8a3a0061ec7c2201f872499edd56e003
valeriahhdez <valeriahhdez@gmail.com>
2024-01-01 15:17:08 -0600

    Save pseudocode

---
+++ file_i_o/shirt.py
@@ -0,0 +1,13 @@
+def main():
+    ...
+
+# Prompt the user to enter an input and output files
+
+# Check if sys.argv has three arguments
+    
+# Check if input and output file have the same valid extension
+    
+# Open input image
+# Resize it
+# Paste template into it
+# Save into outputimage
