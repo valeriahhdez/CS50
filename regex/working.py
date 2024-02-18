050a2d2c85b52fec2b9acdd4fc2d8b357bedf378
valeriahhdez <valeriahhdez@gmail.com>
2024-02-17 20:20:42 -0600

    Add parse_time func

--- regex/working.py
+++ regex/working.py
@@ -1,46 +1,45 @@
 import re
-import sys
-
 
 def main():
     print(convert(input("Hours: ")))
 
 
-def convert(s):
-    """Converts 12-hour time into a 24-hour format
-    Args:
-    - input_string: a string entered by the user containing 12-hour time
-    Returns:
-    - entered time converted into a 24-hour format
-    Raises: 
-    - ValueError: if input is an invalid time, for example, 12:60 AM, 13:00 PM"""
-    # after the colon : numbers can only be 00 to 59
-    # the colon is optional
-    # 1 AM to 11 AM == 1:00 AM to 11:00 AM == 1:00 to 11:00 (24-hour)
-    # 1 PM to 11 PM == 1:00 PM to 11: PM == 13:00 to 23:00 (24-hour)
-    # 12 AM == 12:00 AM == 00:00 (24-hour)
-    # 12 PM == 12:00 PM == 12:00 (24-hour)
-
-    #first check the first number and if the string has a colon
-    match = re.search(r'(..?)(:?)(..?) (AM|PM) (?:to) (..?)(:?)(..?) (AM|PM)', s)
-    # return len(match)
-    try:      
-
-        # match  = re.findall(r'^([1-9]|[0-9]):?([0-5][0-9])$', s)
-
-        # match = re.findall(r'^[1-9]|1[0-2] AM|PM$|^[1-9]|1[0-2]:[0-5][0-9] AM|PM$', s)
-        
-        if match:
-            full_time = match.group(9)
-            return full_time
-        else:
-            return ("No valid time format found.")
-
-    except ValueError:
-        return "Not a valid time"
+def convert(time_range):
+    pattern = r"[\s|to]+"  # Split on "+" or "-"
+    split_list = re.split(pattern,time_range)
+    start_time = split_list[0:2] # returns a list containing the start time and period (len = 2)
+    end_time = split_list[2:] # returns a list containing the end time and period (len = 2)
+    start_dict, end_dict = parse_time(start_time), parse_time(end_time)
 
 
+    
+    
 
+def parse_time(time_str):
+    time_dict = {}
+    try:
+        hour, minutes = time_str[0].split(':')
+        period = time_str[-1]
+        time_dict = {"hour": int(hour), "minutes": int(minutes), "period": period}
+        # print(len(time_dict))
+        # return time_dict
+    except:
+        hour, period = time_str[0], time_str[1]
+        time_dict = {"hour": int(hour), "minutes": 0, "period": period}
+        # print(len(time_dict))
+
+    # hour_new_format = 0
+    if 1 <= time_dict["period"] <= 12 and (time_dict["minutes"] < 60): 
+        if time_dict["period"] == 'AM':
+            if time_dict["hour"] == 12:
+                time_dict["hour"] = 0
+        elif time_dict["period"] == 'PM':
+            if 1 <= time_dict["hour"] <= 11: 
+                time_dict["hour"] = time_dict["hour"] + 12
+    else:
+        raise ValueError
+    
+    return time_dict
 
 
 if __name__ == "__main__":
